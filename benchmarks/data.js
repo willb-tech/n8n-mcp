f0ca6fe3e5988b70768c03f8072d57a25a903c46
github-action-benchmark <github@users.noreply.github.com>
2025-10-02 07:41:39 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 4854a50854003072814c59828720b6d646292e10

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758381732377,
+  "lastUpdate": 1759390899745,
   "repoUrl": "https://github.com/willb-tech/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "range": "0.4194",
             "unit": "ms",
             "extra": "15061 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4854a50854003072814c59828720b6d646292e10",
+          "message": "Merge pull request #244 from czlonkowski/feature/webhook-error-execution-guidance\n\nfeat: enhance webhook error messages with execution guidance",
+          "timestamp": "2025-10-01T12:08:49+02:00",
+          "tree_id": "6b499b925c568797822462bd7941a791b38b8f18",
+          "url": "https://github.com/willb-tech/n8n-mcp/commit/4854a50854003072814c59828720b6d646292e10"
+        },
+        "date": 1759390899302,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0191,
+            "range": "0.3084",
+            "unit": "ms",
+            "extra": "52339 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1761,
+            "range": "0.5627999999999997",
+            "unit": "ms",
+            "extra": "315 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0046,
+            "range": "0.2783",
+            "unit": "ms",
+            "extra": "215268 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0662,
+            "range": "0.28890000000000005",
+            "unit": "ms",
+            "extra": "15100 ops/sec"
           }
         ]
       }
